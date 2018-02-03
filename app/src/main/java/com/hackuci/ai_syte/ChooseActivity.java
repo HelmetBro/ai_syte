17f66a50d5cc37c58a2d55c9dcd09c4d4a0a4e64
helmetbro <ericparsons@live.com>
2018-02-02 23:35:06 -0800

    background and icons working

--- app/src/main/java/com/hackuci/ai_syte/ChooseActivity.java
+++ app/src/main/java/com/hackuci/ai_syte/ChooseActivity.java
@@ -9,20 +9,37 @@ import android.view.WindowManager;
 
 public class ChooseActivity extends AppCompatActivity {
 
+    View decorView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+        decorView = getWindow().getDecorView();
 
         /* HIDES STATUS BAR */
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
         ActionBar actionBar;
         if((actionBar = getActionBar()) != null)
             actionBar.hide();
         /* HIDES STATUS BAR */
 
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        // HIDES NOTIFICATION BAR
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+        // HIDES NOTIFICATION BAR
 
     }
 }
