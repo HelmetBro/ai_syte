05a8e4f2cf2a0db0b8394fbb407cc8402fe2c2a5
helmetbro <ericparsons@live.com>
2018-02-03 08:26:25 -0800

    WORKINGGG

--- app/src/main/java/com/hackuci/ai_syte/ChooseActivity.java
+++ app/src/main/java/com/hackuci/ai_syte/ChooseActivity.java
@@ -1,16 +1,35 @@
 package com.hackuci.ai_syte;
 
 import android.app.ActionBar;
+import android.app.Fragment;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class ChooseActivity extends AppCompatActivity {
 
+    // define the variable that will show the gallery images
+    ImageView imageViewGallery;
     View decorView;
 
     @Override
@@ -25,6 +44,14 @@ public class ChooseActivity extends AppCompatActivity {
             actionBar.hide();
         /* HIDES STATUS BAR */
 
+        // connect the variable to the images_proj.xml
+        imageViewGallery = findViewById(R.id./*image view type*/);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new Fragment())
+                    .commit();
+        }
+
         ImageButton cameraButton = findViewById(R.id.camera);
         cameraButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
@@ -33,6 +60,15 @@ public class ChooseActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    // This function is called after clicking the Browse From Gallery button
+    public boolean onButtonClicked(View view) {
+
+        // access the images gallery
+        Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 2);
+        return true;
     }
 
     @Override
@@ -51,5 +87,58 @@ public class ChooseActivity extends AppCompatActivity {
         }
         // HIDES NOTIFICATION BAR
 
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            File f = new File(Environment.getExternalStorageDirectory().toString());
+            if(f != null){
+                Toast.makeText(getApplication().getApplicationContext(), " not null ", Toast.LENGTH_LONG).show();
+            }
+            try {
+                Bitmap bitmap;
+                BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+
+                bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                        bitmapOptions);
+
+                imageViewGallery.setImageBitmap(bitmap);
+
+                String path = android.os.Environment
+                        .getExternalStorageDirectory()+File.separator;
+                f.delete();
+                OutputStream outFile;
+                File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+                try {
+                    outFile = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                    outFile.flush();
+                    outFile.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == 2) {
+
+            Uri selectedImage = data.getData();
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor c = getApplication().getContentResolver().query(selectedImage, filePath, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePath[0]);
+            String picturePath = c.getString(columnIndex);
+            if(picturePath.startsWith("/")) picturePath = picturePath.substring(1);
+            c.close();
+            Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+            Drawable d = new BitmapDrawable(thumbnail);
+            imageViewGallery.setBackground(d);
+        }
     }
 }
