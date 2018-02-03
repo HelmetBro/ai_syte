357b8e9e68fc5024d7320c2e11e40c1aea7c6a49
helmetbro <ericparsons@live.com>
2018-02-02 22:27:16 -0800

    initial commit

---
+++ app/src/main/java/com/hackuci/ai_syte/ChooseActivity.java
@@ -0,0 +1,28 @@
+package com.hackuci.ai_syte;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class ChooseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+
+        /* HIDES STATUS BAR */
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        ActionBar actionBar;
+        if((actionBar = getActionBar()) != null)
+            actionBar.hide();
+        /* HIDES STATUS BAR */
+
+
+    }
+}
